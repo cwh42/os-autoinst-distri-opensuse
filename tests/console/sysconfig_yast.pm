89dec3121c4802db5b6f7d4e8020aae34826fd98
openQA web UI <openqa@subbotin>
2015-07-08 17:30:44 +0200

    New test for checking value of PKGMGR_INSTALL_RECOMMENDED in /etc/sysconfig/yast2

---
+++ tests/console/sysconfig_yast.pm
@@ -0,0 +1,20 @@
+use base "consoletest";
+use strict;
+use testapi;
+
+# Check value of PKGMGR_INSTALL_RECOMMENDED in /etc/sysconfig/yast2
+sub run() {
+    my $self = shift;
+    
+    script_run("grep '^PKGMGR_INSTALL_RECOMMENDED=' /etc/sysconfig/yast2 > /dev/$serialdev");
+
+    if (get_var("NORECOMMENDS")) {
+        wait_serial("PKGMGR_INSTALL_RECOMMENDED=\"no\"", 20) || die "PKGMGR_INSTALL_RECOMMENDED should be 'no'";
+    }
+    else {
+        wait_serial("PKGMGR_INSTALL_RECOMMENDED=\"yes\"", 20) || die "PKGMGR_INSTALL_RECOMMENDED should be 'yes'";
+    }
+}
+
+1;
+# vim: set sw=4 et:
